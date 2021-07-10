f892b33be462c672448a1f1cbaad3d13638ba7f9
FyodorovAndrey <nvfredy@gmail.com>
2021-07-10 15:46:16 +0500

    Working with db migration

---
+++ src/main/resources/db/migration/V2__AddMessages.sql
@@ -0,0 +1,4 @@
+insert into message (data) values ('Message1');
+insert into message (data) values ('Message2');
+insert into message (data) values ('Message3');
+insert into message (data) values ('Message4');
---
+++ src/main/resources/db/migration/V3__CreaUserTable.sql
@@ -0,0 +1,6 @@
+create table user_table (
+    id bigserial primary key,
+    username varchar(60),
+    password varchar(60),
+    role varchar(60)
+);
